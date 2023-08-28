d394d119866f6e37674ae20af35b8e2cdef99429
SacFeve <antmorin374@outlook.com>
2023-08-28 15:31:00 -0400

    Ajout des membres de l'équipe

--- Team.py
+++ Team.py
@@ -3,7 +3,7 @@ class Team:
         self.win = 0
         self.lose = 0
         self.winrate = 0
-        self.members = ['SacFeve']
+        self.members = ['Bouillon' ,'TR0PHEE', 'MTC Paradox', 'Mister Wubz', 'Jean Francois', 'Sac De Papier', 'Bengadon']
 
     def calculer_winrate(self):
         self.winrate = round(self.win / (self.win + self.lose), 2)
